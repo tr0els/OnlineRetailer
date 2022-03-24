206349b5f41c5a91a65371c71e6f26345975adf4
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2022-03-24 15:06:47 +0100

    Customer mostly finished; maybe needs some more validation in create/edit

--- CustomerApi/Controllers/CustomersController.cs
+++ CustomerApi/Controllers/CustomersController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CustomerApi.Data;
+using CustomerApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace CustomerApi.Controllers
 {
@@ -7,5 +10,79 @@ namespace CustomerApi.Controllers
     [ApiController]
     public class CustomersController : ControllerBase
     {
+        private IRepository<Customer> repository;
+
+        public CustomersController(IRepository<Customer> repo)
+        {
+            repository = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return repository.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetCustomer")]
+        public IActionResult Get(int id)
+        {
+            var item = repository.Get(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Customer customer)
+        {
+            if (customer == null)
+            {
+                return BadRequest();
+            }
+
+            var newCustomer = repository.Add(customer);
+
+            return CreatedAtRoute("GetCustomer", new { id = newCustomer.Id }, newCustomer);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Customer customer)
+        {
+            if (customer == null || customer.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var modifiedCustomer = repository.Get(id);
+
+            if (modifiedCustomer == null)
+            {
+                return NotFound();
+            }
+
+            modifiedCustomer.Name = customer.Name;
+            modifiedCustomer.Email = customer.Email;
+            modifiedCustomer.Phone = customer.Phone;
+            modifiedCustomer.BillingAddress = customer.BillingAddress;
+            modifiedCustomer.ShippingAddress = customer.ShippingAddress;
+            modifiedCustomer.CreditStanding = customer.CreditStanding;
+
+            repository.Edit(modifiedCustomer);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (repository.Get(id) == null)
+            {
+                return NotFound();
+            }
+
+            repository.Remove(id);
+            return new NoContentResult();
+        }
     }
 }
--- CustomerApi/Models/Customer.cs
+++ CustomerApi/Models/Customer.cs
@@ -8,6 +8,8 @@
         public string Phone { get; set; }
         public string BillingAddress { get; set; }
         public string ShippingAddress { get; set; }
+        // CreditStanding could presently be implemented as a bool property instead,
+        // however using an enum gives us the opportunity to add more credit levels later
         public CreditStanding CreditStanding { get; set; }
     }
 
